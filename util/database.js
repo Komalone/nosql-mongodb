63cbdb97e6431eb5848062f8cbfe5cf66b8052db
KomalOne <komalmandal1998@gmail.com>
2024-03-31 19:52:20 +0530

    making post data

---
+++ util/database.js
@@ -0,0 +1,24 @@
+const mongodb = require('mongodb');
+const mongoClient= mongodb.MongoClient;
+
+let _db;
+const mongoConnect=(callback)=>{
+    mongoClient.connect('mongodb+srv://komal123:komal123@atlascluster.qquzra1.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster')
+.then(client =>{
+    console.log('connected')
+    _db=client.db();
+    callback()
+})
+.catch(err =>{
+    console.log(err);
+    throw err
+})
+};
+
+const getDb = ()=>{
+    if(_db) return _db;
+    throw "No database found"
+}
+
+exports.mongoConnect= mongoConnect;
+exports.getDb= getDb;
